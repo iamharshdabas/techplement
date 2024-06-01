f6f30f068a2846b6e914bda95de675a0251c7dd1
Harsh Dabas <iamharshdabas@gmail.com>
2024-06-02 01:38:39 +0530

    fix(quotes): cross origin resource sharing

--- week-1/quotes-server/index.js
+++ week-1/quotes-server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
